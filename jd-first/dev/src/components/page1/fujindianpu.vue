a2ea6ceec641b8385237c17812d23d5e63abe816
xiaopan-p <1208233545@qq.com>
2020-04-01 12:31:08 +0800

    完成jd静态移动端适配
    Signed-off-by: xiaopan-p <1208233545@qq.com>

---
+++ jd-first/dev/src/components/page1/fujindianpu.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="app">
+      <h3>附近店铺</h3>
+      <hr>
+      <p>
+        <span @click="fn(index)" v-for="(item ,index) in list" :class="{color : index ===selectindex}">{{item}}</span>
+        <span @click="fn(index)" class="span-s">筛选∠</span>
+      </p>
+      <ul class="xuan">
+        <li>减免运费</li>
+        <li>高优门店</li>
+        <li>叠加红包</li>
+        <li>首单优惠</li>
+      </ul>
+      <div class="bottom">
+        <div class="b-left"><img src="../../../../vue项目素材/jd/f6440d3b288500ae.png"/></div>
+        <div class="b-right">
+          <h3>山姆会员商店 （双井）</h3>
+          <li class="span-ccc"><span class="span-orange">❤❤❤❤❤❤</span><span class="span-orange">4.8</span><span>月售2千+</span><span class="r-span">102分钟</span><span class="r-span">4.6公里</span></li>
+          <li  class="span-ccc"><span>起送￥0</span><span>基础费用￥5</span><span class="r-span span1">达达专送</span></li>
+          <li class="span-red"><span>领100元券</span><span>领10元运费券</span><span>领88元叠加券</span><span class="r-span span2" style="color: #000">></span></li>
+          <li  class="span-ccc"><span>返礼</span><span>指定联合利华商品满39返5元生鲜券</span></li>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="b-left"><img src="../../../../vue项目素材/jd/f6440d3b288500ae.png"/></div>
+        <div class="b-right">
+          <h3>山姆会员商店 （双井）</h3>
+          <li class="span-ccc"><span class="span-orange">❤❤❤❤❤❤</span><span class="span-orange">4.8</span><span>月售2千+</span><span class="r-span">102分钟</span><span class="r-span">4.6公里</span></li>
+          <li  class="span-ccc"><span>起送￥0</span><span>基础费用￥5</span><span class="r-span span1">达达专送</span></li>
+          <li class="span-red"><span>领100元券</span><span>领10元运费券</span><span>领88元叠加券</span><span class="r-span span2" style="color: #000">></span></li>
+          <li  class="span-ccc"><span>返礼</span><span>指定联合利华商品满39返5元生鲜券</span></li>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "fujindianpu",
+      data(){
+          return{
+            list:['综合排序','销量','距离'],
+            selectindex :0
+          }
+      },
+      methods:{
+          fn(index){
+            this.selectindex = index
+          }
+      }
+    }
+</script>
+
+<style scoped>
+.app{
+  width: 1242px;
+  height: 1284px;
+  border-radius: 30px 30px 0 0;
+  background: white;
+}
+  h3{
+    font-size: 6vw;
+    text-align: left;
+    padding-left: 30px;
+    padding-top: 30px;
+  }
+  p{
+    text-align: left;
+  }
+  p span{
+    font-size: 5vw;
+    padding-left: 30px;
+  }
+  .span-s{
+    float: right;
+    padding-top: 30px;
+    padding-right: 30px;
+  }
+  .color{
+    color: greenyellow;
+  }
+  .xuan{
+    height: 150px;
+  }
+  .xuan li{
+    width: 260px;
+    height: 100px;
+    background: #cccccc7a;
+   margin-left: 40px;
+    text-align: center;
+    line-height: 100px;
+    margin-top: 30px;
+  }
+  .bottom{
+    width: 1242px;
+    height: 425px;
+  }
+  .b-left{
+     width: 174px;
+     height: 174px;
+     float: left;
+   }
+  .b-left img{
+    width: 174px;
+    height: 174px;
+    margin-left: 30px;
+    margin-top: 50px;
+  }
+.b-right{
+  width: 1000px;
+  height: 360px;
+  float: right;
+  border-bottom: 1px solid #ccc;
+}
+  .b-right li{
+    width: 100%;
+    height: 50px;
+    text-align: left;
+    line-height: 50px;
+  }
+  .b-right li span{
+    margin-left: 30px;
+  }
+  .r-span{
+    float: right;
+    margin-right: 50px;
+    padding: 0px 5px;
+  }
+  .span-red{
+    color:red;
+  }
+  .span-ccc{
+    color:#897c7c;
+  }
+ .span-red .span2{
+    transform: rotate(89deg);
+    margin-top: 20px;
+    font-size: 3vw;
+   margin-right: 40px;
+   border: 0;
+  margin-top: -8px;
+  }
+  .span-orange{
+    color: orange;
+  }
+  .span1{
+    border: 1px solid blue;
+    border-radius:10px;
+  }
+  .span-red span{
+    padding: 5px;
+    border: 1px solid red;
+    border-bottom: 0;
+  }
+</style>
---
+++ jd-first/dev/src/components/page1/xianshi-Q.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="app">
+      <h3>限时抢购 <span> 更多></span></h3>
+      <div class="lun">
+        <li>
+          <img src="../../../../vue项目素材/jd/ff8051f35e3e6ae4.png"/>
+          <p>三八妇女节欢迎抢购</p>
+          <div class="l-left"><span class="span1">￥59</span><span class="span2">￥89</span></div>
+          <div class="l-right"><img src="../../../../vue项目素材/jd/seckillQiang.3882994a.png"/></div>
+        </li>
+        <li>
+          <img src="../../../../vue项目素材/jd/ff8051f35e3e6ae4.png"/>
+          <p>三八妇女节欢迎抢购</p>
+          <div class="l-left"><span class="span1">￥59</span><span class="span2">￥89</span></div>
+          <div class="l-right"><img src="../../../../vue项目素材/jd/seckillQiang.3882994a.png"/></div>
+        </li>
+        <li>
+          <img src="../../../../vue项目素材/jd/ff8051f35e3e6ae4.png"/>
+          <p>三八妇女节欢迎抢购</p>
+          <div class="l-left"><span class="span1">￥59</span><span class="span2">￥89</span></div>
+          <div class="l-right"><img src="../../../../vue项目素材/jd/seckillQiang.3882994a.png"/></div>
+        </li>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "xianshi-Q"
+    }
+</script>
+
+<style scoped>
+.app{
+  width: 1242px;
+  height: 760px;
+  background: white;
+  border-radius: 50px;
+  margin-bottom: 20px;
+}
+  h3{
+    text-align: left;
+    padding-top: 20px;
+    font-size: 6vw;
+    padding-left: 30px;
+  }
+  h3 span{
+    display: block;
+    font-size: 3vw;
+   float: right;
+    padding-right: 30px;
+  }
+  .lun{
+    width: 100%;
+    height: 600px;
+  }
+  .lun li{
+    width: 358px;
+    height: 450px;
+    border-radius: 30px;
+    margin-left: 30px;
+    margin-top: 30px;
+    float: left;
+  }
+  .lun img{
+    width: 358px;
+    height: 358px;
+    border-radius: 30px;
+  }
+  .lun p{
+    margin-top: -70px;
+    font-size: 3vw;
+  }
+  .l-left{
+    float: left;
+
+  }
+  .l-left span{
+    display: block;
+    width: 50px;
+    height: 60px;
+    line-height: 50px;
+  }
+  .l-right{
+    float: right;
+  }
+  .l-right img{
+    width: 100px;
+    height: 100px;
+  }
+  .span1{
+    color: red;
+  }
+  .span2{
+    color:#ccc;
+    text-decoration: line-through;
+  }
+</style>
